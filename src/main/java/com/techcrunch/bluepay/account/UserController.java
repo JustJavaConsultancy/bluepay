f4d8b8ddb8b7eba2ba1e990809df680e9829675e
Akinrinde Akinkunmi <akinrindeakinkunmi@gmail.com>
2025-03-18 15:38:53 +0100

    Dynamically set the username on the header

--- src/main/java/com/techcrunch/bluepay/account/UserController.java
+++ src/main/java/com/techcrunch/bluepay/account/UserController.java
@@ -1,11 +1,8 @@
 package com.techcrunch.bluepay.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
@@ -13,9 +10,11 @@ public class UserController {
 
     @Autowired
     AuthenticationManager authenticationManager;
+
     @GetMapping("/loginUser/{field}")
-    public String loginUser(@PathVariable String field){
+    @ResponseBody
+    public String loginUser(@PathVariable String field) {
         String fieldValue = (String) authenticationManager.get(field);
-        return "<span class=\"position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger\">" + fieldValue + "</span>";
+        return fieldValue; // Return plain text instead of an HTML badge
     }
 }
