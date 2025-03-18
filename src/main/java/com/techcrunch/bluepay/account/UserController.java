4c77748a45c4bac01815a2ea8535e5654d8a512a
Just Java Consultancy <kazeem.akinrinde72@gmail.com>
2025-03-18 15:53:33 +0100

    Added logout link

--- src/main/java/com/techcrunch/bluepay/account/UserController.java
+++ src/main/java/com/techcrunch/bluepay/account/UserController.java
@@ -1,5 +1,7 @@
 package com.techcrunch.bluepay.account;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +18,15 @@ public class UserController {
     public String loginUser(@PathVariable String field) {
         String fieldValue = (String) authenticationManager.get(field);
         return fieldValue; // Return plain text instead of an HTML badge
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+        return "/";
     }
 }
