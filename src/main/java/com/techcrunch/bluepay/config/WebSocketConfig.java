76414ddc393b8e66e7d486f6d5bb2f59b89f9769
Just Java Consultancy <kazeem.akinrinde72@gmail.com>
2025-03-27 17:17:01 +0100

    Modified a few things for the WebSocket

--- src/main/java/com/techcrunch/bluepay/config/WebSocketConfig.java
+++ src/main/java/com/techcrunch/bluepay/config/WebSocketConfig.java
@@ -1,32 +1,25 @@
 package com.techcrunch.bluepay.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Autowired
-    private CustomWebSocketHandler customWebSocketHandler;
-    //@Override
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic"); // Prefix for outgoing messages
         registry.setApplicationDestinationPrefixes("/app"); // Prefix for incoming messages
     }
 
-    //@Override
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS(); // WebSocket endpoint
     }
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(customWebSocketHandler, "/ws/modal")
-                .setAllowedOrigins("*");
-    }
-
 }
